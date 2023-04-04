793e4f24be5876f52a9534af9b1ed910c97c516c
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-04 08:52:46 -0300

    Update

---
+++ OOP/counting_game.rb
@@ -0,0 +1,29 @@
+player.one.lives
+player.two.lives
+
+attr_read :lives1, :lives2
+logic if wrong then lives -= 1
+if lives == 0 then game over
+
+
+
+class question (loop here)
+
+player turn and two numbers
+attr_accessor :num1, :num2
+logic add the numbers to find the answers
+puts the numbers for the other player
+attr_accessor :answers
+logic to test for answers
+puts the results
+logic again to count lives
+
+repeat
+
+player is for keeping track of turns
+lives is for keeping track of the lives
+question is for creating the questions.
+
+player is for storing the player data one vs two
+lives get modfied based on results or answerse
+question will provide and check the answers.
