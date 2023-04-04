1551bd187b2cb7161442a3aed33ce372ac1ea244
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-04 11:36:52 -0300

    Count game code

--- counting_game/player.rb
+++ counting_game/player.rb
@@ -1,11 +1,37 @@
-class Player
+module Player
+
+    class Player
 
   attr_accessor :turn, :lives
 
-  def intialize (turn)
-    
+  def initialize(turn)
     @turn = turn
     @lives = 3
-
   end
 
+  def score
+    puts "Player #{turn}: #{lives} lives remaining."
+  end
+
+  def lost
+    lives == 0
+    end
+
+  end
+end
+
+#test prompts
+require_relative 'player'
+
+player1 = Player::Player.new(1)
+player2 = Player::Player.new(2)
+
+puts player1.turn
+puts player2.turn
+
+puts player2.lives
+
+player1.score
+player1.lives -= 1
+
+puts "Player #{player1.turn}: has #{player1.lives} lives"
--- counting_game/questions.rb
+++ counting_game/questions.rb
@@ -1,12 +1,33 @@
-class questions
+require 'player.rb'
 
-    def guess(answer)
-    
-    while player.lives > 0
-    
-     puts "What does #{@num1} plus #{@num2} equal?"
-    
-     gets.chomps(answer)
-      self.answer = a
-    
-      if a == q_number
+module questions
+    class questions
+
+attr_accessor :answer
+
+   def initialize(answer)
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @answer = num1 + num2
+   end
+
+def check_answer(player_answer)
+
+    if @answer == player_answer
+        puts "That is correct!"
+        return true
+    else 
+        puts "Wrong! The answer was #{@answer}"
+        return false
+    end
+
+    def display_question
+        puts "What is the sum of #{num1} + #{num2} ? "
+    end
+end
+
+
+
+
+
+
